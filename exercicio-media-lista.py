e3baba10cc958c40b1388879c1311f2ffc84aaad
Maycon <mayconfabricio2009@gmail.com>
2025-05-03 15:37:30 -0300

    Atualizando arquivo exerciocio-media-lista.py

--- exercicio-media-lista.py
+++ exercicio-media-lista.py
@@ -2,8 +2,12 @@ from typing import List
 
 valores = [9,4,5,6,8]
 
-
 def calcularMediaLista(valores: List[float]) -> float:
+'''
+Recebe uma lista de valores, calcula a média e imprime 
+com duas casas decimais. Ótimo uso de List[float] do módulo 
+typing e da função sum().
+'''
     return sum(valores) / len(valores)
 
 media = calcularMediaLista(valores)
